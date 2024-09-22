a4d7c7687d83b388e43bbb0f65ef33826f708516
debasis <debasispanda3454@gmail.com>
2024-09-22 13:54:29 +0530

    feat: add-files

---
+++ resources/main.tf
@@ -0,0 +1,8 @@
+# resource "aws_instance" "first-ec2" {
+#   ami           = "ami-0e53db6fd757e38c7" # Mumbai region
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "MyTerraformVM-01"
+#   }
+# }
---
+++ resources/output.tf
@@ -0,0 +1,4 @@
+# output "instance_public_ip" {
+#   description = "The public IP of the created EC2 instance."
+#   value       = aws_instance.first-ec2.public_ip
+# }
