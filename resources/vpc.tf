fa0ca9e8ec9153f8d30d2ecc4319697738bc062f
debasis <debasispanda3454@gmail.com>
2024-10-05 21:35:20 +0530

    feat: delete-vpc

--- resources/vpc.tf
+++ resources/vpc.tf
@@ -1,5 +1,5 @@
 # Create a VPC
-resource "aws_vpc" "VPC" {
-  cidr_block = var.vpc_cidr
-  tags       = var.vpc_tags
-}
+# resource "aws_vpc" "VPC" {
+#   cidr_block = var.vpc_cidr
+#   tags       = var.vpc_tags
+# }
