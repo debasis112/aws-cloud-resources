4ae674cbe72b454203829c08e558dd3f24a7017c
debasis <debasispanda3454@gmail.com>
2024-10-06 00:00:43 +0530

    feat: add-data-file-sm

--- resources/data.tf
+++ resources/data.tf
@@ -5,3 +5,7 @@ data "aws_subnet" "subnet-01" {
     values = ["deb-subnet-01"]
   }
 }
+
+data "aws_secretsmanager_secret_version" "secret-version" {
+  secret_id = data.aws_secretsmanager_secret.acr_spn_credentials_version.id
+}
