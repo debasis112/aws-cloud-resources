15787e2f01f3c0fb0b3aeae1227506b5fe4283d9
debasis <debasispanda3454@gmail.com>
2024-10-06 01:07:04 +0530

    feat: nsg-update-for-all-traffic allow

--- resources/nsg.tf
+++ resources/nsg.tf
@@ -11,25 +11,30 @@
 
 /////////////////////////////////////////////////////////////////
 
+
+///////////////////////////////////////////
+# Security group for allowing HTTPS (TLS) and all outbound traffic
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.vpc-01.id
+  vpc_id      = aws_vpc.vpc-01.id  # Ensure this references a valid VPC
   tags        = var.network_security_group_tags
 }
 
+# Ingress rule for allowing HTTPS (TLS) traffic from the internet (0.0.0.0/0)
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.vpc-01.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"   # Allow traffic from the internet
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
   tags              = var.network_security_group_tags
 }
 
+# Egress rule for allowing all outbound traffic
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  cidr_ipv4         = "0.0.0.0/0"   # Allow outbound traffic to any destination
+  ip_protocol       = "-1"          # All protocols and ports
   tags              = var.network_security_group_tags
 }
