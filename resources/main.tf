58e0e4faed5b8e0dd8e97179b12fc9d1778a824a
debasis <debasispanda3454@gmail.com>
2024-10-06 00:47:05 +0530

    feat: version-ecs-iam-role

--- resources/main.tf
+++ resources/main.tf
@@ -50,7 +50,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
-    Version = "2024-10-06",
+    Version = "2012-10-17",
     Statement = [{
       Action = "sts:AssumeRole",
       Effect = "Allow",
