a775eae6c0f00ef4d9bad7eecb3c3149d4be3a88
debasis <debasispanda3454@gmail.com>
2024-10-05 21:25:56 +0530

    feat: initial commit

--- resources/variables.tf
+++ resources/variables.tf
@@ -1,9 +1,9 @@
-# variable "vpc_cidr" {
-#   description = "CIDR for VPC"
-#   type        = string
-# }
+variable "vpc_cidr" {
+  description = "CIDR for VPC"
+  type        = string
+}
 
-# variable "vpc_tags" {
-#   description = "Tags for VPC"
-#   type        = map(any)
-# }
+variable "vpc_tags" {
+  description = "Tags for VPC"
+  type        = map(any)
+}
