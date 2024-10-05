d94cd58911432a19dbd48a3346f011a16fe18b10
debasis <debasispanda3454@gmail.com>
2024-10-06 01:35:10 +0530

    add-cert

---
+++ resources/certificate.tf
@@ -0,0 +1,13 @@
+# Request an ACM certificate
+resource "aws_acm_certificate" "cert-01" {
+  domain_name       = "meditracks.in" # Replace with your actual domain name
+  validation_method = "DNS"           # Use DNS validation
+
+  subject_alternative_names = [
+    "www.meditracks.in", # Optional: add additional domain names if needed
+  ]
+
+  tags = {
+    Name = "MyCertificate-meditracks.in"
+  }
+}
