5683b7e49da2a7b1a78b601ad36a239c9292a896
debasis <debasispanda3454@gmail.com>
2024-10-06 01:16:29 +0530

    up

--- resources/nsg.tf
+++ resources/nsg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "allow_tls" {
 # Ingress rule for allowing HTTPS (TLS) traffic from the internet (0.0.0.0/0)
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"   # Allow traffic from the internet
+  cidr_ipv4         = var.nsg_ipv4_cidr   # Allow traffic from the internet
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -19,7 +19,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 # Egress rule for allowing all outbound traffic
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"   # Allow outbound traffic to any destination
+  cidr_ipv4         = var.nsg_ipv4_cidr   # Allow outbound traffic to any destination
   ip_protocol       = "-1"          # All protocols and ports
   tags              = var.network_security_group_tags
 }
--- resources/terraform.tfvars
+++ resources/terraform.tfvars
@@ -1,5 +1,7 @@
 vpc_cidr = "10.0.0.0/16"
 
+nsg_ipv4_cidr = "0.0.0.0/0"
+
 subnet_cidr = "10.0.1.0/24"
 
 vpc_tags = {
