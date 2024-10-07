bc60ef4f095388e2bffe8fd9dc2ac67141430247
debasis <debasispanda3454@gmail.com>
2024-10-07 18:11:03 +0530

    delete-all

--- resources/route-table.tf
+++ resources/route-table.tf
@@ -1,8 +1,8 @@
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.vpc-01.id
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.vpc-01.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gateway-01.id
-  }
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gateway-01.id
+#   }
+# }
