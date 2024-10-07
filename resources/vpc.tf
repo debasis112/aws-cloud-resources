c3488da6505b58d8a52a647dc325f0f0126028b2
debasis <debasispanda3454@gmail.com>
2024-10-07 16:47:33 +0530

    test-main

--- resources/vpc.tf
+++ resources/vpc.tf
@@ -5,17 +5,17 @@ resource "aws_vpc" "vpc-01" {
 }
 
 # Create a subnets
-resource "aws_subnet" "subnet-01" {
-  vpc_id            = aws_vpc.vpc-01.id
-  cidr_block        = var.subnet_cidr
-  tags              = var.subnet_tags
-  availability_zone = var.subnet_zone
-}
-
-# // For multiple subnets
 # resource "aws_subnet" "subnet-01" {
-#   count             = 2
 #   vpc_id            = aws_vpc.vpc-01.id
-#   cidr_block        = "10.0.${count.index}.0/24"
-#   availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
+#   cidr_block        = var.subnet_cidr
+#   tags              = var.subnet_tags
+#   availability_zone = var.subnet_zone
 # }
+
+// For multiple subnets
+resource "aws_subnet" "subnet-01" {
+  count             = 2
+  vpc_id            = aws_vpc.vpc-01.id
+  cidr_block        = "10.0.${count.index}.0/24"
+  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
+}
