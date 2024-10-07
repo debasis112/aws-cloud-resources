b70d1c9a36651b98b61e38369b6bc5a66d52b695
debasis <debasispanda3454@gmail.com>
2024-10-07 19:59:58 +0530

    update-subnets

--- resources/vpc.tf
+++ resources/vpc.tf
@@ -6,8 +6,8 @@
 
 # Create multiple VPCs
 resource "aws_vpc" "vpc-01" {
-  count       = var.vpc_count
-  cidr_block  = "10.0.${count.index}.0/16"
-
+  count      = var.vpc_count
+  cidr_block = "10.${count.index}.0.0/16"  
+  # Vary the second octet 10.0.0.0 to 10.0.255.255 giving a total of 65,536 IP addresses in this range.
   tags = merge(var.vpc_tags, { Name = "deb-vpc-${count.index + 1}" })  # Unique name for each VPC
 }
