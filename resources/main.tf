bd46d61b4efe221a83ab7b20805c0b10deda926e
debasis <debasispanda3454@gmail.com>
2024-10-07 16:42:01 +0530

    feat: update-zone-subnet-*

--- resources/main.tf
+++ resources/main.tf
@@ -40,7 +40,7 @@ resource "aws_ecs_service" "web_service-01" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = [aws_subnet.subnet-01.id]
+    subnets          = [aws_subnet.subnet-01[*].id]
     assign_public_ip = true
     security_groups  = [aws_security_group.ecs_sg-01.id]
 
