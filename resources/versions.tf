94f88e76311091cca48e6b5faa1a3c023808f247
debasis <debasispanda3454@gmail.com>
2024-10-07 16:27:55 +0530

    feat: update-files-ecs

--- resources/versions.tf
+++ resources/versions.tf
@@ -1,9 +1,23 @@
+# terraform {
+#   required_version = "~> 1.9.5"
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "5.68.0"
+#     }
+#   }
+# }
+
 terraform {
   required_version = "~> 1.9.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.68.0"
+    }
+    godaddy = {
+      source  = "n3integration/godaddy"
+      version = "~> 1.5"
     }
   }
 }
