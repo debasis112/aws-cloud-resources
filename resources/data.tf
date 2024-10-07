526ab6bb4fa85c5beccdb9c54975a2489b7b102f
debasis <debasispanda3454@gmail.com>
2024-10-07 20:31:31 +0530

    feat: update

--- resources/data.tf
+++ resources/data.tf
@@ -1,20 +1,21 @@
-# # For specific subnet of name tag
-# data "aws_subnet" "subnet-01" {
-#   filter {
-#     name   = "tag:Name"
-#     values = ["deb-subnet-01"]
-#   }
-# }
-
-# Data block to fetch existing subnets by name
+# For specific subnet of name tag
 data "aws_subnet" "subnet-01" {
   count = var.subnet_count
-
   filter {
     name   = "tag:Name"
-    values = ["${var.subnet_base_name}-${count.index + 1}"]  # Dynamic subnet name using variable
+    values = ["deb-subnet-${count.index + 1}"]
   }
 }
+
+# Data block to fetch existing subnets by name
+# data "aws_subnet" "subnet-01" {
+#   count = var.subnet_count
+
+#   filter {
+#     name   = "tag:Name"
+#     values = ["${var.subnet_base_name}-${count.index + 1}"]  # Dynamic subnet name using variable
+#   }
+# }
 
 # data "aws_secretsmanager_secret_version" "secret-id" {
 #   secret_id = aws_secretsmanager_secret.acr_spn_credentials.id
