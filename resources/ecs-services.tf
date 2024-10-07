331538acbef5158db4f6483fa2d463f1b1d27f4a
debasis <debasispanda3454@gmail.com>
2024-10-07 22:22:47 +0530

    feat: create-sg-ecs

--- resources/ecs-services.tf
+++ resources/ecs-services.tf
@@ -1,5 +1,6 @@
 # Define an ECS service to run the task
 resource "aws_ecs_service" "web_service-01" {
+  //count             = var.subnet_count
   name            = "web-service"
   cluster         = aws_ecs_cluster.web_cluster.id
   task_definition = aws_ecs_task_definition.web_task-01.arn
@@ -7,7 +8,7 @@ resource "aws_ecs_service" "web_service-01" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = aws_subnet.subnet-01[*].id
+    subnets          = aws_subnet.subnet-01[count.index].id
     assign_public_ip = true
     security_groups  = [aws_security_group.ecs_sg-01.id]
 
