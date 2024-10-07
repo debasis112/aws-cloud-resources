994921460450b7848d346cb6b6964bc18d202b90
debasis <debasispanda3454@gmail.com>
2024-10-07 19:51:02 +0530

    update-subnets

--- resources/subnets.tf
+++ resources/subnets.tf
@@ -6,10 +6,21 @@
 #   availability_zone = var.subnet_zone
 # }
 
-# For multiple subnets
-resource "aws_subnet" "subnet-01" {
-  count             = var.subnet_count
-  vpc_id            = aws_vpc.vpc-01.id
-  cidr_block        = "10.0.${count.index}.0/24"
-  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
+# # For multiple subnets
+# resource "aws_subnet" "subnet-01" {
+#   count             = var.subnet_count
+#   vpc_id            = aws_vpc.vpc-01.id
+#   cidr_block        = "10.0.${count.index}.0/24"
+#   availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
+# }
+
+# Create subnets in each VPC
+resource "aws_subnet" "subnet" {
+  count             = var.subnet_count * var.vpc_count  # Total subnets across all VPCs
+  vpc_id            = aws_vpc.vpc-01[count.index / var.subnet_count].id  # Reference the appropriate VPC
+  cidr_block        = "10.${count.index / var.subnet_count}.${count.index % var.subnet_count}.0/24"  # Adjust as necessary
+  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index % var.subnet_count)
+
+  # Use tags similar to the previous example
+  tags = merge(var.subnet_tags, { Name = "deb-subnet-${count.index + 1}" }) 
 }
