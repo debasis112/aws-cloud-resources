3cec0595c689c1ec1d8524ad935f074de8f54b0c
debasis <debasispanda3454@gmail.com>
2024-10-07 18:12:01 +0530

    delete-all

--- resources/route-table-associate.tf
+++ resources/route-table-associate.tf
@@ -1,5 +1,5 @@
-resource "aws_route_table_association" "udr_subnet_association" {
-  count          = 2
-  subnet_id      = aws_subnet.subnet-01[count.index].id
-  route_table_id = aws_route_table.public.id
-}
+# resource "aws_route_table_association" "udr_subnet_association" {
+#   count          = 2
+#   subnet_id      = aws_subnet.subnet-01[count.index].id
+#   route_table_id = aws_route_table.public.id
+# }
