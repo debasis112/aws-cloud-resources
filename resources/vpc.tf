acd0dd6a99cff4d6e6dc4a863a3d5b2c892adfdf
debasis <debasispanda3454@gmail.com>
2024-10-07 21:09:27 +0530

    feat: create-vpc-subnet

--- resources/vpc.tf
+++ resources/vpc.tf
@@ -4,10 +4,10 @@
 #   tags       = var.vpc_tags
 # }
 
-# # Create multiple VPCs
-# resource "aws_vpc" "vpc-01" {
-#   count      = var.vpc_count
-#   cidr_block = "10.${count.index}.0.0/16"
-#   # Vary the second octet 10.0.0.0 to 10.0.255.255 giving a total of 65,536 IP addresses in this range.
-#   tags = merge(var.tags, { Name = "${var.vpc_base_name}-${count.index + 1}" })  # Unique name for each VPC
-# }
+# Create multiple VPCs
+resource "aws_vpc" "vpc-01" {
+  count      = var.vpc_count
+  cidr_block = "10.${count.index}.0.0/16"
+  # Vary the second octet 10.0.0.0 to 10.0.255.255 giving a total of 65,536 IP addresses in this range.
+  tags = merge(var.tags, { Name = "${var.vpc_base_name}-${count.index + 1}" })  # Unique name for each VPC
+}
