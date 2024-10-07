a2144a67e0e3a1eb67ad51f40c1da5994dc8b340
debasis <debasispanda3454@gmail.com>
2024-10-07 20:34:39 +0530

    data-up

--- resources/data.tf
+++ resources/data.tf
@@ -1,19 +1,8 @@
-# For specific subnet of name tag
-data "aws_subnet" "subnet-01" {
-
-  filter {
-    name   = "tag:Name"
-    values = ["deb-subnet"]
-  }
-}
-
-# Data block to fetch existing subnets by name
+# # For specific subnet of name tag
 # data "aws_subnet" "subnet-01" {
-#   count = var.subnet_count
-
 #   filter {
 #     name   = "tag:Name"
-#     values = ["${var.subnet_base_name}-${count.index + 1}"]  # Dynamic subnet name using variable
+#     values = ["deb-subnet-01"]
 #   }
 # }
 
