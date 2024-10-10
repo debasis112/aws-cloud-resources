f8c32bac2a4e161626eb47d48c409848886c71c8
debasis <debasispanda3454@gmail.com>
2024-10-10 20:13:58 +0530

    feat: delete-acm

--- resources/certificate.tf
+++ resources/certificate.tf
@@ -1,11 +1,11 @@
-# Request an ACM certificate
-resource "aws_acm_certificate" "cert-01" {
-  domain_name       = "meditracks.in" # Replace with your actual domain name
-  validation_method = "DNS"           # Use DNS validation
+# # Request an ACM certificate
+# resource "aws_acm_certificate" "cert-01" {
+#   domain_name       = "meditracks.in" # Replace with your actual domain name
+#   validation_method = "DNS"           # Use DNS validation
 
-  subject_alternative_names = [
-    "www.meditracks.in", # Optional: add additional domain names if needed
-  ]
+#   subject_alternative_names = [
+#     "www.meditracks.in", # Optional: add additional domain names if needed
+#   ]
 
-  tags = var.ssl_cert_tags
-}
+#   tags = var.ssl_cert_tags
+# }
