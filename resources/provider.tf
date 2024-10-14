ef2f592be5dddd9b9e6fee5ea878530f733162dc
debasis <debasispanda3454@gmail.com>
2024-10-14 17:40:00 +0530

    feat: create-all-resources-for-ecs

--- resources/provider.tf
+++ resources/provider.tf
@@ -1,13 +1,13 @@
 # Configure the AWS Provider
-# provider "aws" {
-#   region = "ap-south-1" # Mumbai
-# }
+provider "aws" {
+  region = "ap-south-1" # Mumbai
+}
 
 # For ECR we have to use US region as for free tier
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
+# provider "aws" {
+#   alias  = "us_east_1"
+#   region = "us-east-1"
+# }
 
 # provider "godaddy" {
 #   key    = var.domain_access_key # Optional if you want to automate DNS
