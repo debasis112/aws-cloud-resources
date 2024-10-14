b0ec7bf288f009037df3b5711e25d187f3733201
debasis <debasispanda3454@gmail.com>
2024-10-14 13:56:50 +0530

    feat: create-IAM-policy-pub-repo-ecr

--- resources/ecr-public-policy.tf
+++ resources/ecr-public-policy.tf
@@ -1,5 +1,5 @@
 # ECR Public repo policy
 resource "aws_ecrpublic_repository_policy" "public-repo-policy" {
   repository_name = aws_ecrpublic_repository.public-repo-01.repository_name
-  policy          = data.aws_iam_policy_document.example.json
+  policy          = data.aws_iam_policy_document.iam-policy-01.json
 }
