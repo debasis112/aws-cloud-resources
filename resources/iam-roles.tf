bc02f65f25878f92a095a970fb81421f060acfe0
debasis <debasispanda3454@gmail.com>
2024-10-14 19:21:47 +0530

    feat: create-ecs-task-service

--- resources/iam-roles.tf
+++ resources/iam-roles.tf
@@ -19,3 +19,16 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+
+resource "aws_iam_policy_attachment" "ecs_execution_policy" {
+  name       = "ecs_execution_policy_attachment"
+  roles      = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_policy_attachment" "ecs_loadbalancer_access" {
+  name       = "ecs_lb_access_policy_attachment"
+  roles      = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
+}
