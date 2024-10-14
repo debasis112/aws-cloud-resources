84336f02e85117b7bbbb5d3ba4b549a03ca3c33e
debasis <debasispanda3454@gmail.com>
2024-10-14 14:06:12 +0530

    feat: create-IAM-policy-pub-repo-ecr

--- resources/ecr-public-policy.tf
+++ resources/ecr-public-policy.tf
@@ -1,5 +1,6 @@
 # ECR Public repo policy
 resource "aws_ecrpublic_repository_policy" "public-repo-policy" {
+  provider        = aws.us_east_1  # Reference the aliased provider
   repository_name = aws_ecrpublic_repository.public-repo-01.repository_name
   policy          = data.aws_iam_policy_document.iam-policy-01.json
 }
