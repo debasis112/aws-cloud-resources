daef2be5b715355529ab4df3a06cd5fe344d5c75
debasis <debasispanda3454@gmail.com>
2024-10-14 14:43:05 +0530

    feat: ecr-private-repo-policy

--- resources/ecr-iam-policy.tf
+++ resources/ecr-iam-policy.tf
@@ -5,35 +5,35 @@ resource "aws_ecrpublic_repository_policy" "public-repo-policy" {
   policy          = data.aws_iam_policy_document.iam-policy-01.json
 }
 
-# # ECR Private repo policy
-# resource "aws_ecr_repository_policy" "private-repo-policy" {
-#   provider   = aws.us_east_1 # Reference the aliased provider
-#   repository = aws_ecr_repository.private-repo-01.name
-#   policy     = data.aws_iam_policy_document.iam-policy-01.json
-# }
+# ECR Private repo policy
+resource "aws_ecr_repository_policy" "private-repo-policy" {
+  provider   = aws.us_east_1 # Reference the aliased provider
+  repository = aws_ecr_repository.private-repo-01.name
+  policy     = data.aws_iam_policy_document.iam-policy-01.json
+}
 
-# # ECR Private repository image life cycle policy
-# resource "aws_ecr_lifecycle_policy" "example" {
-#   provider   = aws.us_east_1 # Reference the aliased provider
-#   repository = aws_ecr_repository.private-repo-01.name
+# ECR Private repository image life cycle policy
+resource "aws_ecr_lifecycle_policy" "example" {
+  provider   = aws.us_east_1 # Reference the aliased provider
+  repository = aws_ecr_repository.private-repo-01.name
 
-#   policy = <<EOF
-# {
-#     "rules": [
-#         {
-#             "rulePriority": 1,
-#             "description": "Expire images older than 14 days",
-#             "selection": {
-#                 "tagStatus": "untagged",
-#                 "countType": "sinceImagePushed",
-#                 "countUnit": "days",
-#                 "countNumber": 14
-#             },
-#             "action": {
-#                 "type": "expire"
-#             }
-#         }
-#     ]
-# }
-# EOF
-# }
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
