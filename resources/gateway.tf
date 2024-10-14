3340eb8e3e55bba8ac604e8ccd6e5f5719d096d1
debasis <debasispanda3454@gmail.com>
2024-10-14 17:33:33 +0530

    feat: create-all-resources-for-ecs

--- resources/gateway.tf
+++ resources/gateway.tf
@@ -1,6 +1,6 @@
-# # Internet Gateway for VPC
-# resource "aws_internet_gateway" "gateway-01" {
-#   count      = var.vpc_count
-#   vpc_id = aws_vpc.vpc-01[count.index].id
-#   tags = merge(var.tags, { Name = "${var.base_name}-gw-01" })
-# }
+# Internet Gateway for VPC
+resource "aws_internet_gateway" "gateway-01" {
+  count      = var.vpc_count
+  vpc_id = aws_vpc.vpc-01[count.index].id
+  tags = merge(var.tags, { Name = "${var.base_name}-gw-01" })
+}
