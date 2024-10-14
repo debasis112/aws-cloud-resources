9ed5e790e77ef88823f23dd228fe387c093b6d6d
debasis <debasispanda3454@gmail.com>
2024-10-14 19:03:28 +0530

    feat: create-ecs-task-service

--- resources/app-lb-tg.tf
+++ resources/app-lb-tg.tf
@@ -1,8 +1,8 @@
-# # Application Load Balancer Taget Group
-# resource "aws_lb_target_group" "app_tg-01" {
-#   name     = "app-tg-01"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = aws_vpc.vpc-01[*].id
-#   tags = merge(var.tags, { Name = "${var.base_name}-lb-tg-01" })
-# }
+# Application Load Balancer Taget Group
+resource "aws_lb_target_group" "app_tg-01" {
+  name     = "app-tg-01"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc-01[*].id
+  tags = merge(var.tags, { Name = "${var.base_name}-lb-tg-01" })
+}
--- resources/ecs-services.tf
+++ resources/ecs-services.tf
@@ -12,3 +12,27 @@
 #     security_groups  = aws_security_group.ecs_sg-01[*].id
 #   }
 # }
+
+resource "aws_ecs_service" "web_service-01" {
+  name            = "web-page-01"
+  cluster         = aws_ecs_cluster.web_cluster.id
+  task_definition = aws_ecs_task_definition.my_task.arn
+  desired_count   = 3
+  iam_role        = aws_iam_role.ecs_task_execution_role.arn
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_tg-01.arn
+    container_name   = "web-page"
+    container_port   = 8080
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [ap-south-1a, ap-south-1b]"
+  }
+}
