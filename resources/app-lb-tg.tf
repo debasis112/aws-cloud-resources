bb61f00c1799d12b0d633423ae169187333d20d7
debasis <debasispanda3454@gmail.com>
2024-10-14 19:10:02 +0530

    feat: create-ecs-task-service

--- resources/app-lb-tg.tf
+++ resources/app-lb-tg.tf
@@ -3,6 +3,6 @@ resource "aws_lb_target_group" "app_tg-01" {
   name     = "app-tg-01"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.vpc-01[*].id
+  vpc_id   = aws_vpc.vpc-01[0].id
   tags = merge(var.tags, { Name = "${var.base_name}-lb-tg-01" })
 }
