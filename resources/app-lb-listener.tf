1208f700c8078dd80091bb8aeed3f2a2d38905c5
debasis <debasispanda3454@gmail.com>
2024-10-14 20:47:45 +0530

    feat: delete-tg-lb

--- resources/app-lb-listener.tf
+++ resources/app-lb-listener.tf
@@ -13,13 +13,13 @@
 
 
 # 7. Create Load Balancer Listener
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.app_lb.arn
-  port              = 80
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "front_end" {
+#   load_balancer_arn = aws_lb.app_lb.arn
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.ecs_target_group.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.ecs_target_group.arn
+#   }
+# }
