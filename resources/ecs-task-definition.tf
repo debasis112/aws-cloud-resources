9785e6d1562c215c635378300b2df88f7a06a619
debasis <debasispanda3454@gmail.com>
2024-10-14 18:50:31 +0530

    feat: delete-ecs-task-service

--- resources/ecs-task-definition.tf
+++ resources/ecs-task-definition.tf
@@ -1,48 +1,48 @@
-# # Define the task definition with the ACR image
-# resource "aws_ecs_task_definition" "web_task-01" {
-#   family = "web-task"
-#   container_definitions = jsonencode([
-#     {
-#       name      = "my-container",
-#       # image     = "debacrregistry.azurecr.io/project-work:latest",
-#       image     = "debasis1999/project-work:latest"
-#       cpu       = 512,
-#       memory    = 1024, # Container memory is 1024MB
-#       essential = true,
-#       portMappings = [
-#         {
-#           containerPort = 80,
-#           hostPort      = 80
-#         }
-#       ]
-#     }
-#   ])
+# # # Define the task definition with the ACR image
+# # resource "aws_ecs_task_definition" "web_task-01" {
+# #   family = "web-task"
+# #   container_definitions = jsonencode([
+# #     {
+# #       name      = "my-container",
+# #       # image     = "debacrregistry.azurecr.io/project-work:latest",
+# #       image     = "debasis1999/project-work:latest"
+# #       cpu       = 512,
+# #       memory    = 1024, # Container memory is 1024MB
+# #       essential = true,
+# #       portMappings = [
+# #         {
+# #           containerPort = 80,
+# #           hostPort      = 80
+# #         }
+# #       ]
+# #     }
+# #   ])
 
+# #   requires_compatibilities = ["FARGATE"]
+# #   network_mode             = "awsvpc"
+# #   cpu                      = "512"  # Match the container's CPU or higher
+# #   memory                   = "1024" # Increase task memory to match container memory (at least 1024MB)
+# #   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+# # }
+
+
+# resource "aws_ecs_task_definition" "my_task" {
+#   family                   = "my-task"
+#   network_mode             = "awsvpc" # Required for Fargate
 #   requires_compatibilities = ["FARGATE"]
-#   network_mode             = "awsvpc"
-#   cpu                      = "512"  # Match the container's CPU or higher
-#   memory                   = "1024" # Increase task memory to match container memory (at least 1024MB)
+#   cpu                      = "256" # vCPU units
+#   memory                   = "512" # Memory in MiB
 #   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+#   task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
+
+#   container_definitions = jsonencode([{
+#     name      = "my-container"
+#     image     = "022499026373.dkr.ecr.us-east-1.amazonaws.com/private-project-work:v1.0.0" # Replace with your ECR image
+#     essential = true
+#     portMappings = [{
+#       containerPort = 80 # Change this to your application's port
+#       hostPort      = 80
+#       protocol      = "tcp"
+#     }]
+#   }])
 # }
-
-
-resource "aws_ecs_task_definition" "my_task" {
-  family                   = "my-task"
-  network_mode             = "awsvpc" # Required for Fargate
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256" # vCPU units
-  memory                   = "512" # Memory in MiB
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([{
-    name      = "my-container"
-    image     = "022499026373.dkr.ecr.us-east-1.amazonaws.com/private-project-work:v1.0.0" # Replace with your ECR image
-    essential = true
-    portMappings = [{
-      containerPort = 80 # Change this to your application's port
-      hostPort      = 80
-      protocol      = "tcp"
-    }]
-  }])
-}
