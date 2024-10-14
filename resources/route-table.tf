867c6495d648fd86b39e84415c05a831674e05ee
debasis <debasispanda3454@gmail.com>
2024-10-14 17:46:25 +0530

    feat: create-all-resources-for-ecs

--- resources/route-table.tf
+++ resources/route-table.tf
@@ -1,9 +1,9 @@
 # Route table creation
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.vpc-01[*].id
+  vpc_id = aws_vpc.vpc-01[0].id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gateway-01[*].id
+    gateway_id = aws_internet_gateway.gateway-01[0].id
   }
 }
