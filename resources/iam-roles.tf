35de093776ea9c4eb0946fb17145d368355e27ff
debasis <debasispanda3454@gmail.com>
2024-10-19 00:27:41 +0530

    feat: iam-policy-up

--- resources/iam-roles.tf
+++ resources/iam-roles.tf
@@ -129,33 +129,25 @@ resource "aws_iam_role" "apprunner_ecr_access" {
 #   })
 # }
 
-# For ECR to pull image from private repo
 resource "aws_iam_policy" "ecr_access_policy" {
   name = "AppRunner-ECR-Cross-Region-Policy"
 
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ecr:GetAuthorizationToken",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:GetRepositoryPolicy",
-                "ecr:DescribeRepositories",
-                "ecr:ListImages",
-                "ecr:DescribeImages",
-                "ecr:BatchGetImage",
-                "ecr:GetLifecyclePolicy",
-                "ecr:GetLifecyclePolicyPreview",
-                "ecr:ListTagsForResource",
-                "ecr:DescribeImageScanFindings"
-            ],
-            "Resource": "*"
-        }
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetAuthorizationToken",
+          "ecr:DescribeRepositories"  # Optional: Add if you need to describe repositories
+        ],
+        "Resource" : "arn:aws:ecr:us-east-1:022499026373:repository/private-project-work"
+      }
     ]
-})
+  })
 }
 
 # For ECR to pull image from private repo
