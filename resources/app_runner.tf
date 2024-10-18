d9ebf408f06d1281abcae29cd3986020c080b62f
debasis <debasispanda3454@gmail.com>
2024-10-18 23:21:07 +0530

    feat: create-private-app-run-02

--- resources/app_runner.tf
+++ resources/app_runner.tf
@@ -74,28 +74,57 @@
 #   }
 # }
 
-# For Public repo in ECR
-resource "aws_apprunner_service" "web_app_public" {
-  service_name = "my-public-app-public"
+# # For Public repo in ECR as Auto deploy cannot done for public
+# resource "aws_apprunner_service" "web_app_public" {
+#   service_name = "my-public-app-public"
+
+#   source_configuration {
+#     image_repository {
+#       image_identifier      = "public.ecr.aws/l6f8k7r6/public-project-work:v1.0.0"  # Update with your public ECR image URI
+#       image_repository_type = "ECR_PUBLIC"  # Specify that this is an ECR Public image
+#       image_configuration {
+#         port = "80"
+#       }
+#     }
+#   }
+
+#   instance_configuration {
+#     cpu    = "1024"
+#     memory = "2048"
+#   }
+
+#   # auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration.my_auto_scaling.arn
+
+#   tags = {
+#     Name = "MyAppRunnerService"
+#   }
+# }
+
+resource "aws_apprunner_service" "web_app_private" {
+  service_name = "my-private-app"
 
   source_configuration {
     image_repository {
-      image_identifier      = "public.ecr.aws/l6f8k7r6/public-project-work:v1.0.0"  # Update with your public ECR image URI
-      image_repository_type = "ECR_PUBLIC"  # Specify that this is an ECR Public image
+      image_identifier      = "022499026373.dkr.ecr.us-east-1.amazonaws.com/private-project-work:v1.0.0"  # ECR repo in us-east-1
+      image_repository_type = "ECR"
+      
       image_configuration {
-        port = "80"
+        runtime_environment_variables = {
+          AWS_DEFAULT_REGION = "ap-south-1"  # Use the region where App Runner is running
+        }
       }
     }
+
+    # Attach the IAM role for cross-region access
+    authentication_configuration {
+      access_role_arn = aws_iam_role.apprunner_ecr_access.arn
+    }
+    
+    auto_deployments_enabled = true
   }
 
   instance_configuration {
     cpu    = "1024"
     memory = "2048"
-  }
-
-  # auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration.my_auto_scaling.arn
-
-  tags = {
-    Name = "MyAppRunnerService"
   }
 }
