5a9d459485d252a2d36183c8a3281ffe2165ba87
debasis <debasispanda3454@gmail.com>
2024-10-19 00:24:54 +0530

    feat: iam-policy-up

--- resources/iam-roles.tf
+++ resources/iam-roles.tf
@@ -134,27 +134,28 @@ resource "aws_iam_policy" "ecr_access_policy" {
   name = "AppRunner-ECR-Cross-Region-Policy"
 
   policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetAuthorizationToken"
-        ],
-        Resource = "arn:aws:ecr:us-east-1:022499026373:repository/private-project-work"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ecr:GetAuthorizationToken"
-        ],
-        "Resource" : "*"
-      }
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:GetRepositoryPolicy",
+                "ecr:DescribeRepositories",
+                "ecr:ListImages",
+                "ecr:DescribeImages",
+                "ecr:BatchGetImage",
+                "ecr:GetLifecyclePolicy",
+                "ecr:GetLifecyclePolicyPreview",
+                "ecr:ListTagsForResource",
+                "ecr:DescribeImageScanFindings"
+            ],
+            "Resource": "*"
+        }
     ]
-  })
+})
 }
 
 # For ECR to pull image from private repo
