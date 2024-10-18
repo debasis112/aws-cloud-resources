f939debe811351f1f284c132525c4c5682f19df7
debasis <debasispanda3454@gmail.com>
2024-10-18 23:42:54 +0530

    feat: create-private-repo-02

--- resources/ecr.tf
+++ resources/ecr.tf
@@ -13,21 +13,6 @@ resource "aws_ecrpublic_repository" "public-repo-01" {
   tags = merge(var.tags, { Name = "${var.base_name}-ecr-public-01" })
 }
 
-# ECR Public repository creation
-resource "aws_ecrpublic_repository" "public-repo-02" {
-  # provider        = aws.us_east_1
-  repository_name = "public-project-work"
-
-  catalog_data {
-    about_text        = "Public repository in ECR."
-    architectures     = ["ARM"]
-    description       = "This repository isa public repository in ECR for storing image for my project."
-    operating_systems = ["Linux"]
-  }
-
-  tags = merge(var.tags, { Name = "${var.base_name}-ecr-public-01" })
-}
-
 # ECR Private repository creation.
 resource "aws_ecr_repository" "private-repo-01" {
   provider             = aws.us_east_1
@@ -38,4 +23,16 @@ resource "aws_ecr_repository" "private-repo-01" {
     scan_on_push = true
   }
   tags = merge(var.tags, { Name = "${var.base_name}-ecr-private-01" })
+}
+
+# ECR Private repository creation.
+resource "aws_ecr_repository" "private-repo-02" {
+  # provider             = aws.us_east_1
+  name                 = "private-project-work"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = merge(var.tags, { Name = "${var.base_name}-ecr-private-02" })
 }
